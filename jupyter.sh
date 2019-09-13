b3bc72a888e96c33f2f10ba43ac97fd61a300240
Saran Sankar <31500520+stykes@users.noreply.github.com>
2019-09-14 00:02:59 +0530

    Delete jupyter.sh

--- jupyter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-URL=$(jupyter notebook list)
-URL=$(echo $URL | cut -d' ' -f4)
-FILE=$(echo Programming/CSE/Projects/Workspace/Heart\ Disease\ Visualization/Sophisticated\ Visualizations.ipynb)
-python -mwebbrowser $(echo $URL)
-URL=$(echo $URL | cut -d'?' -f1)
-URL=$($URL$FILE)
-python -mwebbrowser $(echo $URL)
